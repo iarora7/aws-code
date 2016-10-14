f7da11ae22ee9524bde66b1a3be472460bd08ad2
Arora <iarora@usc.edu>
2016-10-14 12:22:31 -0700

    Service to add address in DB

--- flassk.py
+++ flassk.py
@@ -1,32 +1,29 @@
 from flask import Flask, request
 import psycopg2
 
-# Demo flask service api
-
 app = Flask(__name__)
 
 conn = psycopg2.connect(database="test1", user="postgres", password="Welcome@123", host="54.67.5.124", port="5432")
-
+# print ("Opened database successfully")
 
 @app.route("/")
 def hello():
     return "Hello, world!"
 
-@app.route("/next")
-def next_route():
-    return "This is next page"
-
 @app.route("/addr/<addr>", methods=['GET', 'POST'])
 def save_Address(addr):
     cur = conn.cursor()
-    cur.execute("INSERT INTO HOUSES (ADDRESS) VALUES (%s)", addr);
+    cur.execute("INSERT INTO HOUSES (ADDRESS) VALUES (%s)", (addr,));
+    conn.commit()
     return ('my new address: %s' % addr)
 
+# sample url:
+# http://ec2-52-53-202-11.us-west-1.compute.amazonaws.com:8080/login?username=isha&password=13
 @app.route("/login", methods=['GET'])
 def login_fn():
-    return request.args.get[username]
-
-
+    name =  request.args.get('username')
+    password = request.args.get('password')
+    return (name + ":" + password)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)

008fd5ec4f71bf55d62f716dc0f920d5ccb468c1
Arora <iarora@usc.edu>
2016-10-22 18:44:54 -0700

    cleaning directories

--- subleased/subapp/__init__.py
+++ subleased/subapp/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
+#import psycopg2
+#conn = psycopg2.connect(database="test1", user="postgres", password="Welcome@123", host="54.67.5.124", port="5432")
+
 import subapp.addAddress
 import subapp.login
 import subapp.searchHouseAvailable
